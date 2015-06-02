c78655630dbc3ff6cc3cf6c3a85b1bedc3f8c11d
cadaver123 <cadaver123@gmail.com>
2015-06-02 16:19:44 +0200

    configured sjf

--- src/main/java/com/todo/initializer/AppInitializer.java
+++ src/main/java/com/todo/initializer/AppInitializer.java
@@ -2,17 +2,18 @@ package com.todo.initializer;
 
 
 
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 public class AppInitializer implements WebApplicationInitializer {
 
@@ -22,6 +23,7 @@ public class AppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         WebApplicationContext context = getContext();
+        
         servletContext.addListener(new ContextLoaderListener(context));
         
         
@@ -34,6 +36,11 @@ public class AppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping(MAPPING_URL);
+        
+        ServletRegistration.Dynamic dispatcherJSF = servletContext.addServlet("Faces Servlet", new FacesServlet());
+        dispatcherJSF.setLoadOnStartup(2);
+        dispatcherJSF.addMapping("/jsf/*");
+        dispatcherJSF.addMapping("*.xhtml");
         
         
         
